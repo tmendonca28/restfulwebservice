4bbcf24e30878b012d193a861efefc0032c1c54b
Anthony Mendonca <tmendonca28@gmail.com>
2018-07-03 12:14:55 +0300

    Add a simple get request using Spring boot

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Making the application executable
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,28 @@
+package hello;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+    private final int age;
+
+
+    public Greeting(long id, String content, int age) {
+        this.id = id;
+        this.content = content;
+        this.age = age;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
